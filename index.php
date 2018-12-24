b0a36e22f77efbe4a2111a9234a9c4223050e088
Alisson Renan <alissonalves29@gmail.com>
2018-12-24 12:25:51 -0200

    tela de login

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php 
-
+session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 
 $app = new Slim();
@@ -16,12 +17,14 @@ $app->get('/', function() {
 	$page = new Page();
 	
 	$page->setTpl("index");
-	$page->setTpl("footer");
+	
 
 
 });
 
 $app->get('/admin', function() {
+
+	User::verifyLogin();
     
 	$page = new PageAdmin();
 	
@@ -30,6 +33,33 @@ $app->get('/admin', function() {
 
 
 });
+
+$app->get('/admin/login', function(){
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]
+	);
+
+	$page->setTpl("login");
+});
+
+$app->post('/admin/login', function(){
+
+	User::login($_POST['login'], $_POST['password']);
+
+	header('location: /admin');
+	exit;
+});
+
+$app->get("/admin/logout", function(){
+
+	User::logout();
+
+	header("Location: /admin/login");
+});
+
 
 $app->run();
 
