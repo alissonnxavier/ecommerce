8de266a441f4726e2e855af2073f18c090079dab
Alisson Renan <alissonalves29@gmail.com>
2019-01-16 18:36:36 -0200

    admin - produtos

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ use \Hcode\Page;
 use \Hcode\PageAdmin;
 use \Hcode\Model\User;
 use	\Hcode\Model\Category;
+use \Hcode\Model\Product;
 
 
 $app = new Slim();
@@ -312,6 +313,90 @@ $app->get("/categories/:idcategory", function($idcategory){
 		'category'=>$category->getValues(),
 		'products'=>[]
 	]);
+});
+
+$app->get("/admin/products", function(){
+
+	User::verifyLogin();
+
+	$products = Product::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("products", [
+		"products"=>$products
+	]);
+});
+
+$app->get("/admin/products/create", function(){
+
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("products-create");
+});
+
+$app->post("/admin/products/create", function(){
+
+	User::verifyLogin();
+
+	$product = new Product();
+
+	$product->setData($_POST);
+
+	$product->save();
+
+	header("location: /admin/products");
+	exit;
+});
+
+$app->get("/admin/products/:idproduct", function($idproduct){
+
+	User::verifyLogin();
+
+	$product = new Product();
+
+	$product->get((int)$idproduct);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("products-update", [
+		"product"=>$product->getValues()
+	]);
+});
+
+$app->post("/admin/products/:idproduct", function($idproduct){
+
+	User::verifyLogin();
+
+	$product = new Product();
+
+	$product->get((int)$idproduct);
+
+	$product->setData();
+
+	$product->save();
+
+	$product->setPhoto($_FILES["file"]);
+
+	header("location: /admin/products");
+	exit;
+
+});
+
+$app->get("/admin/products/:idproduct/delete" , function($idproduct){
+
+	User::verifyLogin();
+
+	$product = new Product();
+
+	$product->get((int)$idproduct);
+
+	$product->delete();
+
+	header("location: /admin/products");
+	exit;
 });
 
 $app->run();
